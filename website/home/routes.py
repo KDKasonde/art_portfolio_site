9cb344834bc197779ed000cab2736fceb5bbd8f0
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-02-26 19:38:55 +0000

    add css to generate images on site, and function in js to randomly load them in

--- website/home/routes.py
+++ website/home/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template
-
+from pathlib import Path
+from random import shuffle
+import os
 from website.home import bp
 
 
 @bp.route('/', methods=['GET'])
 def homepage():
-    return render_template("home/homepage.html")
+    gallery_folder = Path(__file__).parent.parent.joinpath("static/assets/gallery/")
+    image_list = os.listdir(gallery_folder)
+    shuffle(image_list)
+    return render_template("home/homepage.html", image_list=image_list)
