f70ef5e65aa5c653d8f8d3950634677001ed01b7
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-04-16 00:27:08 +0100

    add code to redirect user to new page when image is clicked

--- website/__init__.py
+++ website/__init__.py
@@ -11,4 +11,7 @@ def create_website():
     from website.about import bp as about
     website.register_blueprint(about, url_prefix="/about")
 
+    from website.high_quality_view import bp as high_quality_view
+    website.register_blueprint(high_quality_view, url_prefix="/high_quality_view")
+
     return website
--- website/high_quality_view/routes.py
+++ website/high_quality_view/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import os
+from pathlib import Path
 
 from website.high_quality_view import bp
 
 
 @bp.route("/", methods=["GET"])
 def homepage():
+    image = request.args.get('image_id')
+    gallery_folder = Path(__file__).parent.parent.joinpath("static", "assets", "gallery_jpg")
+    image_list = os.listdir(gallery_folder)
+    for image_file in image_list:
+        if image == image_file.split(".")[0]:
+            return redirect(url_for("high_quality_view.homepage", image=image))
+
     return render_template("high_quality_view/homepage.html")
