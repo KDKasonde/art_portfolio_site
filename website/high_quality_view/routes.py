9076c7b35687f4a1e1fa2118f96b009489c37c05
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-04-16 21:38:14 +0100

    add high quality image page

--- website/high_quality_view/routes.py
+++ website/high_quality_view/routes.py
@@ -7,11 +7,19 @@ from website.high_quality_view import bp
 
 @bp.route("/", methods=["GET"])
 def homepage():
+    image = request.args.get('image')
+    print(image)
+    return render_template("high_quality_view/homepage.html", image=image)
+
+
+@bp.route("/getImage", methods=["GET"])
+def get_image():
     image = request.args.get('image_id')
     gallery_folder = Path(__file__).parent.parent.joinpath("static", "assets", "gallery_jpg")
     image_list = os.listdir(gallery_folder)
     for image_file in image_list:
         if image == image_file.split(".")[0]:
+            print(image)
             return redirect(url_for("high_quality_view.homepage", image=image))
 
-    return render_template("high_quality_view/homepage.html")
+    return redirect(url_for("high_quality_view.homepage", image=image))
