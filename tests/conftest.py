3c727d441faf4d73f9994a39c58a12bdb45e9a54
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-08-21 19:05:40 +0100

    set poetry env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,26 @@
 import os
 import tempfile
-
 import pytest
 from mdurocherart import create_app
+from flask_mailman import Mail
 
 
 @pytest.fixture
 def app():
-    db_fd, db_path = tempfile.mkstemp()
 
-    app = create_app({
-        'DATABASE': db_path
-    })
-
-    with app.app_context():
-        init_db()
-        get_db()
+    app = create_app()
 
     yield app
-
-    with app.app_context():
-        close_db()
-    os.close(db_fd)
-    os.unlink(db_path)
 
 
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def mail_client(app):
+    yield Mail(app)
 
 
 @pytest.fixture
