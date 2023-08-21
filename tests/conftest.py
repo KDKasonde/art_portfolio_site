a79556dee0afeb20eee51c0e2693d9d06e2f5562
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-08-21 22:18:17 +0100

    add tests for emails being sent and simple fake smt server code remove hard, small edits such as coded email addr set up configs for full end to end testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,14 @@ from flask_mailman import Mail
 def app():
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     app = create_app(env='test')
 =======
     app = create_app()
 >>>>>>> 2a397a7 (feat: add toml file along with the flask-mailman package, set up poetry env and ensure the package is stable.)
+=======
+    app = create_app(env='test')
+>>>>>>> 83ffea9 (add tests for emails being sent and simple fake smt server code remove hard, small edits such as coded email addr set up configs for full end to end testing)
 
     yield app
 
@@ -24,13 +28,8 @@ def client(app):
 
 @pytest.fixture
 def mail_client(app):
-<<<<<<< HEAD
     with app.app_context():
         yield Mail(app)
-=======
-    yield Mail(app)
->>>>>>> 2a397a7 (feat: add toml file along with the flask-mailman package, set up poetry env and ensure the package is stable.)
-
 
 @pytest.fixture
 def runner(app):
