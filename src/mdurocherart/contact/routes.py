c4890c3d1f9cea514e52e17011dab207cbff856f
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-08-22 11:29:50 +0100

    add front end functionality to get send the email payload to the backend

--- src/mdurocherart/contact/routes.py
+++ src/mdurocherart/contact/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from mdurocherart.contact import bp
 
@@ -6,3 +6,9 @@ from mdurocherart.contact import bp
 @bp.route("/", methods=["GET"])
 def homepage():
     return render_template("contact/homepage.html")
+
+
+@bp.route("/send_email", methods=["POST"])
+def send_email():
+    print(request.json)
+    return jsonify(success=True)
