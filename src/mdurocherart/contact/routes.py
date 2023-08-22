8e04aad13f90c6c1fddd43be355687a232081ee3
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-08-22 12:19:13 +0100

    write end to end for sending an email over googles smt server, need to chck what error gets thrown when 500 daily limit is reached

--- src/mdurocherart/contact/routes.py
+++ src/mdurocherart/contact/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 
 from mdurocherart.contact import bp
+from mdurocherart.contact.models import send_email, format_email
 
 
 @bp.route("/", methods=["GET"])
@@ -8,7 +9,9 @@ def homepage():
     return render_template("contact/homepage.html")
 
 
-@bp.route("/send_email", methods=["POST"])
-def send_email():
-    print(request.json)
+@bp.route("/process_contact", methods=["POST"])
+def process_contact():
+    req = request.json
+    email_obj = format_email(req)
+    status = send_email(email_obj)
     return jsonify(success=True)
--- src/mdurocherart/models.py
+++ src/mdurocherart/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 
 class Email(BaseModel):
     email: EmailStr = Field(...)
     subject: str = Field(...)
     body: str = Field(...)
+    name: Optional[str]
