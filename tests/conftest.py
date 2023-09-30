aaac0c0d54fc736267a32dec120b785831af0f08
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-09-30 16:46:03 +0100

    straggling merge conflicts removal

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,16 +8,7 @@ from flask_mailman import Mail
 @pytest.fixture
 def app():
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     app = create_app(env='test')
-=======
-    app = create_app()
->>>>>>> 2a397a7 (feat: add toml file along with the flask-mailman package, set up poetry env and ensure the package is stable.)
-=======
-    app = create_app(env='test')
->>>>>>> 83ffea9 (add tests for emails being sent and simple fake smt server code remove hard, small edits such as coded email addr set up configs for full end to end testing)
-
     yield app
 
 
