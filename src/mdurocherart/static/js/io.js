52b6a1263b4e9dc2d692100862406ecd15252eb7
Kasonde Kasonde <d.kasonde99@gmail.com>
2023-10-12 23:11:50 +0100

    feat: add go to interactive mode button functionality

--- src/mdurocherart/static/js/io.js
+++ src/mdurocherart/static/js/io.js
@@ -19,131 +19,44 @@ async function makePostRequest(url, payload){
 }
 
 
-async function makeGetRequest(url, payload){
-    let response = await fetch(
-        url = url,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
+async function makeGetRequest(url){
+    try{
+        let response = await fetch(
+            url = url,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+
+        if (response.ok && response.status === 200){
+            return response
         }
-    )
-
-    if (response.ok && response.status === 200){
-        return response
+        throw(
+            new Error(`There was an issue loading the request, ${response.statusText} exited with code: ${response.status}`)
+        )
+    } catch {
+        alert('There was an error processing your request, please contact support for further assistance.')
     }
-    throw(
-        new Error(`There was an issue loading the request, ${response.statusText} exited with code: ${response.status}`)
-    )
 }
 
 
-function trigger(event) {
-const leftBtn = document.getElementById('left-slider-btn');
-    // function to determine which side of the screen the mouse is and
-    // highlight a scroll button for users who do not have a touchpad
-    const rightBtn = document.getElementById('right-slider-btn');
-    const mouseX = event.clientX;
-    const centerOfSlider = (slider_rect.right + slider_rect.left) / 2;
-
-    if ((mouseX <= centerOfSlider)) {
-        if (!leftBtn.disabled) {
-            leftBtn.focus();
-            setTimeout(() => {
-                leftBtn.blur();
-            }, 1000);
-        } else {
-            rightBtn.focus();
-            setTimeout(() => {
-                rightBtn.blur();
-            }, 1000);
-        }
-
+async function redirectFromGet(url){
+    let response = await makeGetRequest(url);
+    if (response.ok){
+        window.location = response.url;
     } else {
-        if (!rightBtn.disabled) {
-            rightBtn.focus();
-            setTimeout(() => {
-                rightBtn.blur();
-            }, 1000);
-        } else {
-            leftBtn.focus();
-            setTimeout(() => {
-                leftBtn.blur();
-            }, 1000);
-        }
+        alert('There was an issue processing your request!')
     }
 }
 
-function disableSliderBtn() {
-    // disables slider buttons when they
-    // is nothing to scroll to
-    const centerImageId = getCenterImage();
-    if (`${centerImageId}` == 0){
-        document.getElementById('left-slider-btn').disabled = true;
-    } else {
-        document.getElementById('left-slider-btn').removeAttribute("disabled");
-    }
 
-    if (`${centerImageId}` == (imagesList.length-1)){
-        document.getElementById('right-slider-btn').disabled = true;
-    } else {
-        document.getElementById('right-slider-btn').removeAttribute("disabled");
-    }
-}
-
-function slideLeft() {
-    // Slide image slider left
-    const centerImageId = getCenterImage();
-    const targetImage = document.getElementById(`${centerImageId-1}`);
-    targetImage.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center'
-    });
-
-    disableSliderBtn();
-}
-
-function slideRight() {
-    // Slide image slider right
-    const centerImageId = parseInt(getCenterImage());
-    const targetImage = document.getElementById(`${centerImageId+1}`);
-    targetImage.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center'
-    });
-
-    disableSliderBtn();
-}
-
-function arrowSlider(event) {
-    // arrow key slider
-    const centerImageId = parseInt(getCenterImage());
-
-    if ((event.which == 37) && (`${centerImageId}` !=0)){
-        /*left key code*/
-        slideLeft()
-    }
-    if ((event.which == 39) && (`${centerImageId}` != (imagesList.length-1))){
-        /*right key code*/
-        slideRight();
-    }
-
-}
-
-
-function Onload(imageList) {
-    resizeSlider()
-    imagesList.forEach(image => {
-        image.addEventListener("click", function(event) {
-            openImage(event);
-            event.stopPropagation();
-        });
-    });
-    leftSliderBtn.addEventListener("click", slideLeft);
-    rightSliderBtn.addEventListener("click", slideRight);
-    document.addEventListener('keydown', arrowSlider);
-    disableSliderBtn();
+async function redirectFromPost(url, payload){
+    let response = await makePostRequest(url, payload);
+    if (response.ok){
+        window.location = response.url;
+    };
+    alert('There was an issue processing your request!')
 }
--- src/mdurocherart/static/js/slider.js
+++ src/mdurocherart/static/js/slider.js
@@ -1,3 +1,112 @@
+function trigger(event) {
+const leftBtn = document.getElementById('left-slider-btn');
+    // function to determine which side of the screen the mouse is and
+    // highlight a scroll button for users who do not have a touchpad
+    const rightBtn = document.getElementById('right-slider-btn');
+    const mouseX = event.clientX;
+    const centerOfSlider = (slider_rect.right + slider_rect.left) / 2;
+
+    if ((mouseX <= centerOfSlider)) {
+        if (!leftBtn.disabled) {
+            leftBtn.focus();
+            setTimeout(() => {
+                leftBtn.blur();
+            }, 1000);
+        } else {
+            rightBtn.focus();
+            setTimeout(() => {
+                rightBtn.blur();
+            }, 1000);
+        }
+
+    } else {
+        if (!rightBtn.disabled) {
+            rightBtn.focus();
+            setTimeout(() => {
+                rightBtn.blur();
+            }, 1000);
+        } else {
+            leftBtn.focus();
+            setTimeout(() => {
+                leftBtn.blur();
+            }, 1000);
+        }
+    }
+}
+
+function disableSliderBtn() {
+    // disables slider buttons when they
+    // is nothing to scroll to
+    const centerImageId = getCenterImage();
+    if (`${centerImageId}` == 0){
+        document.getElementById('left-slider-btn').disabled = true;
+    } else {
+        document.getElementById('left-slider-btn').removeAttribute("disabled");
+    }
+
+    if (`${centerImageId}` == (imagesList.length-1)){
+        document.getElementById('right-slider-btn').disabled = true;
+    } else {
+        document.getElementById('right-slider-btn').removeAttribute("disabled");
+    }
+}
+
+function slideLeft() {
+    // Slide image slider left
+    const centerImageId = getCenterImage();
+    const targetImage = document.getElementById(`${centerImageId-1}`);
+    targetImage.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+    });
+
+    disableSliderBtn();
+}
+
+function slideRight() {
+    // Slide image slider right
+    const centerImageId = parseInt(getCenterImage());
+    const targetImage = document.getElementById(`${centerImageId+1}`);
+    targetImage.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+    });
+
+    disableSliderBtn();
+}
+
+function arrowSlider(event) {
+    // arrow key slider
+    const centerImageId = parseInt(getCenterImage());
+
+    if ((event.which == 37) && (`${centerImageId}` !=0)){
+        /*left key code*/
+        slideLeft()
+    }
+    if ((event.which == 39) && (`${centerImageId}` != (imagesList.length-1))){
+        /*right key code*/
+        slideRight();
+    }
+
+}
+
+
+function Onload(imageList) {
+    resizeSlider()
+    imagesList.forEach(image => {
+        image.addEventListener("click", function(event) {
+            openImage(event);
+            event.stopPropagation();
+        });
+    });
+    leftSliderBtn.addEventListener("click", slideLeft);
+    rightSliderBtn.addEventListener("click", slideRight);
+    document.addEventListener('keydown', arrowSlider);
+    disableSliderBtn();
+}
+
 function checkImages(imageList){
     // Function that takes the list of images being shown
     // and uses the window to distinguish which ones are in view
